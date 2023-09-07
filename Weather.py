ac10ea745141a42663d586e026b6e168da4e5417
awv2804 <atharvarao100@gmail.com>
2023-09-06 21:37:49 -0400

    Fixed api key

--- Weather.py
+++ Weather.py
@@ -3,7 +3,7 @@ import json
 
 
 
-weather_response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=40.4259&lon=-86.9081&appid=36468be155548021577d9192b9d65c6d&units=metric")
+
 packages_json = weather_response.json()
 packages_str = json.dumps(packages_json, indent=2)
 
